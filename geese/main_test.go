1272473e2434b9f18b36bf5116ecb23e6709bf66
borkab <borbala.botond@gmail.com>
2022-04-06 22:56:30 +0200

    for loop is correct now

--- geese/main_test.go
+++ geese/main_test.go
@@ -6,11 +6,21 @@ import (
 )
 
 func TestUngeeser(t *testing.T) {
+	all := Birds{"Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"}
+	geese := Birds{"African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"}
+	want := Birds{"Mallard", "Hook Bill", "Crested", "Blue Swedish"}
 
-	want := nogeese
-
-	if got := Ungeeser(allbirds); !reflect.DeepEqual(got, want) {
-		t.Errorf("got %#v want %#v", got, want)
+	if got := Ungeeser(all, geese); !reflect.DeepEqual(got, want) {
+		t.Fatalf("got %#v want %#v", got, want)
 	}
+}
 
+func TestUngeeser_NoGeeseInAllBirds(t *testing.T) {
+	all := Birds{"A", "B"}
+	geese := Birds{"C", "D", "E", "F"}
+	want := Birds{"A", "B"}
+
+	if got := Ungeeser(all, geese); !reflect.DeepEqual(got, want) {
+		t.Fatalf("got %#v want %#v", got, want)
+	}
 }
